453a1c16283957f8ae439ff82d4d78b3f223e453
santoshpatel4157 <asmisthecat@gmail.com>
2023-03-07 17:37:10 +0530

    done

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,17 +3,22 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <h1>My Blog</h1>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>New Blog</Link>
+        <Link
+          to="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </Link>
       </div>
     </nav>
   );
-}
- 
+};
+
 export default Navbar;
