98f8b1c3bea7fa66c7ebc4223a17f70e3048fd17
santosh patel <109598196+santoshpatel4157@users.noreply.github.com>
2023-03-07 17:00:29 +0530

    Update useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,30 +1,42 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
-  const [pending, setPending] = useState(true);
+  const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
-  useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("we cod't fetch data");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
-          setPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setPending(false);
-        });
-    }, 1000);
-  }, [url]);
 
-  return { data, pending, error };
-};
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        }
+      })
+    }, 1000);
+
+    // abort the fetch
+    return () => abortCont.abort();
+  }, [url])
+
+  return { data, isPending, error };
+}
+ 
 export default useFetch;
